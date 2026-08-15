abd403f78ae381fafdaf8e56dd3ef9763ccaca43
404existential <grilledonion2@gmail.com>
2026-08-15 15:27:03 +0530

    Load the latest museum interface upgrade

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ window.MOUT_CONFIG = {
 };
 (function(){
   var css=document.createElement('link');css.rel='stylesheet';css.href='enhancements.css';document.head.appendChild(css);
-  var s=document.createElement('script');s.src='enhancements.js';s.onload=function(){var p=document.createElement('script');p.src='finalpatch.js';document.body.appendChild(p)};document.body.appendChild(s);
+  var s=document.createElement('script');s.src='enhancements.js';s.onload=function(){var p=document.createElement('script');p.src='finalpatch.js';p.onload=function(){var u=document.createElement('script');u.src='museum-upgrade.js';document.body.appendChild(u)};document.body.appendChild(p)};document.body.appendChild(s);
 })();
