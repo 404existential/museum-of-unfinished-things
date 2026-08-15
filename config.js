0f263b334b80aaac2f26cf0856138e631a5934cc
404existential <grilledonion2@gmail.com>
2026-08-15 15:41:58 +0530

    Remove layered script loader to stop page freezes

--- config.js
+++ config.js
@@ -3,7 +3,3 @@ window.MOUT_CONFIG = {
   supabaseUrl: 'https://oyoacuakzwsvxmzcckot.supabase.co',
   supabaseAnonKey: 'sb_publishable_9E_Hf168dkeWJIQbhgHCKg_DEPsWQc7'
 };
-(function(){
-  var css=document.createElement('link');css.rel='stylesheet';css.href='enhancements.css';document.head.appendChild(css);
-  var s=document.createElement('script');s.src='enhancements.js';s.onload=function(){var p=document.createElement('script');p.src='finalpatch.js';p.onload=function(){var u=document.createElement('script');u.src='museum-upgrade.js';u.onload=function(){var f=document.createElement('script');f.src='museum-final.js';document.body.appendChild(f)};document.body.appendChild(u)};document.body.appendChild(p)};document.body.appendChild(s);
-})();
